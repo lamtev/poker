c19927298c023c75dc0040a2eaef66f2b405d149
Anton Lamtev <antonlamtev@gmail.com>
2016-11-15 21:37:19 +0300

    fix

--- core/src/main/java/com/lamtev/poker/core/states/WinnersDeterminingPokerState.java
+++ core/src/main/java/com/lamtev/poker/core/states/WinnersDeterminingPokerState.java
@@ -1,13 +1,23 @@
 package com.lamtev.poker.core.states;
 
 import com.lamtev.poker.core.api.Poker;
+import com.lamtev.poker.core.model.Bank;
+import com.lamtev.poker.core.model.Cards;
+import com.lamtev.poker.core.model.Players;
 import com.lamtev.poker.core.states.exceptions.GameIsOverException;
 
 class WinnersDeterminingPokerState implements PokerState {
 
-    WinnersDeterminingPokerState(Poker poker) {
-        //TODO do action
+    private Poker poker;
+    private Players players;
+    private Cards commonCards;
+    private Bank bank;
 
+    WinnersDeterminingPokerState(Poker poker) {
+        this.poker = poker;
+        this.players = poker.getPlayers();
+        this.commonCards = poker.getCommonCards();
+        this.bank = poker.getBank();
     }
 
     @Override
